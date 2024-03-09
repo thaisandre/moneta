b93e3f0b0e55666486b6c7a1f51add34dddbde43
thaisandre <thaisandre.sc@gmail.com>
2024-03-09 08:36:49 -0300

    ajustando arquivo de propriedades

--- src/main/java/com/tta/moneta/utils/JsonUtils.java
+++ src/main/java/com/tta/moneta/utils/JsonUtils.java
@@ -1,7 +1,6 @@
 package com.tta.moneta.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
@@ -9,7 +8,6 @@ public final class JsonUtils {
 
     public static String toJson(Object object) {
         ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
-        objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
         try {
             return objectMapper.writeValueAsString(object);
         } catch (JsonProcessingException e) {
